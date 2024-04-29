d1f791da607e6d145579bd40c471994a54708931
YCDM03 <ycdm03@gmail.com>
2024-04-29 14:05:45 +0900

    (update)/config/token.js

--- js/config/token.js
+++ js/config/token.js
@@ -1,3 +1,4 @@
 //options.Authorization: value
-const token = "1";
+const token =
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODdlNjM3ZTFkZDQzMThmYjUzMTRjODNmMDQzNGMxMiIsInN1YiI6IjY2MjhiN2Y3MTc2YTk0MDE0YzgxYTg1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LWD-SlE-6FP5DHc3JEdFWnh1EDCuvVKZYZzoCAJvZiQ";
 export default { token };
