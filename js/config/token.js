41467ed56c5732c9228f208ffc55fa0847b9e0e1
YCDM03 <ycdm03@gmail.com>
2024-04-29 12:38:35 +0900

    test3

--- js/config/token.js
+++ js/config/token.js
@@ -1,4 +1,1 @@
-const token =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODdlNjM3ZTFkZDQzMThmYjUzMTRjODNmMDQzNGMxMiIsInN1YiI6IjY2MjhiN2Y3MTc2YTk0MDE0YzgxYTg1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LWD-SlE-6FP5DHc3JEdFWnh1EDCuvVKZYZzoCAJvZiQ";
 
-export default { token };
