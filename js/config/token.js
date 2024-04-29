204208998db3ad0fec9f71dcd21837d66dd23f7d
YCDM03 <ycdm03@gmail.com>
2024-04-29 12:46:09 +0900

    (fix)git ignore

--- js/config/token.js
+++ js/config/token.js
@@ -1,3 +1,4 @@
-const token = "";
-
+//options.Authorization: value
+const token =
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjODdlNjM3ZTFkZDQzMThmYjUzMTRjODNmMDQzNGMxMiIsInN1YiI6IjY2MjhiN2Y3MTc2YTk0MDE0YzgxYTg1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LWD-SlE-6FP5DHc3JEdFWnh1EDCuvVKZYZzoCAJvZiQ";
 export default { token };
